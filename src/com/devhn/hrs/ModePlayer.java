0f6c9e0d3407df57e331556046e354892f0c8f3d
DevHN <devhn0@gmail.com>
2018-05-30 23:08:34 +0800

    Add ModePlayer.java
    - Add more contents in  ModePlayer.java

--- src/com/devhn/hrs/ModePlayer.java
+++ src/com/devhn/hrs/ModePlayer.java
@@ -1,11 +1,42 @@
 package com.devhn.hrs;
 
+import java.util.Scanner;
+
 class ModePlayer {
     void play(String mode) {
+        Scanner scanner = new Scanner(System.in);
+
         if (mode.equals("섞기")) {
-            System.out.println("앙녕하세여");
+            System.out.println("섞을 이름을 입력하여 주세요. (이름은 ',' 콤마로 구분합니다.)");
+
+            String[] nameinput = scanner.nextLine().split(", ");
+
+            System.out.println("입력되었습니다. 다음은 뽑기 결과입니다.");
+            System.out.println(getRandArr(nameinput));
+
         } else if (mode.equals("뽑기")) {
-            System.out.println("앙뇽하세여");
+            System.out.println("뽑을 이름을 입력하여 주세요. (이름은 ',' 콤마로 구분합니다.)");
+
+            String[] nameinput = scanner.nextLine().split(", ");
+
+            System.out.println("입력되었습니다. 다음은 뽑기 결과입니다.");
+
+            String fixedname = getRandArr(nameinput);
+
+            System.out.println("뽑힌 사람: " + fixedname);
+
         }
+    }
+
+    public static String getRandArr(String[] arr) {
+        return arr[getRand(arr.length - 1)];
+    }
+
+    public static int getRand(int n) {
+        return getRand(0, n);
+    }
+
+    public static int getRand(int from, int to) {
+        return (int)(Math.random() * (Math.abs(to - from) + 1)) + Math.min(from, to);
     }
 }
